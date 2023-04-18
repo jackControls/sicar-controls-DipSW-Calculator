fc6c5798b6e22a1377649621fd056fb032a20503
JackS <sample@email.com>
2023-04-17 22:33:19 -0500

    initial

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,7 @@
-import { View, Text } from '@tarojs/components'
+import { View, Input, Button, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
+
 
 export default function Index() {
 
@@ -8,9 +9,13 @@ export default function Index() {
     console.log('Page loaded.')
   })
 
+  var decimal
+
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+      <Text>DIP Switch Calculator</Text>
+      <Input type='number' maxlength={3} value={decimal} placeholder='Type in the F_Dest_Addr' focus/>
+      <Button plain type='primary'>Caculate</Button>
     </View>
   )
 }
